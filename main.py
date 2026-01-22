d14b6630a29d20cebe64ba96e7898d6f1326a315
chaseusejavascripts <chasedaddy4@gmail.com>
2026-01-22 12:04:49 -0500

    Fix relationship experience evaluation logic

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-value = 8
+value =8
 time =int(input("enter how long your relationship was:"))
-if value >= 2:
+if value <=2:
     print("You haven't had any real experience")
-if value <= 2:
-    print("You have had any some experience")
-if value <= 6:
-    print("You have a decent experience")
-if value <= 12:
-    print("You have alot of real experience")
+if value >=2:
+    print("You have had real experience")
 
-print("Thank you for using my decision program!")
+
